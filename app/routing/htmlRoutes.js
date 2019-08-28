056429f4e8d4c19fc846e1847f94e986d51e9dd9
Troy Garcia <tg1212@outlook.com>
2019-08-28 17:32:49 -0400

    Fixed minor bugs

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -8,12 +8,10 @@ var path = require("path");
 
 module.exports = function(app) {
   app.get("/survey", function(req, res) {
-       console.log("Survey");
     res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   app.get("*", function(req, res) {
-     console.log("Home");
     res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
